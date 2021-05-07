fe4d15ed367c7e69719ff625080540847845d633
Documentation Push <nipreps@gmail.com>
2021-05-07 10:56:59 +0000

    doc(update) [skip ci]

--- fix_topup-coeff-scaling/_static/documentation_options.js
+++ fix_topup-coeff-scaling/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.1.dev59',
+    VERSION: '2.0.1.dev61',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
